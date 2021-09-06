8f8f9ff587359726045a2ebaae1b9ff468c89a8d
wwren <rdding@student.unimelb.edu.au>
2021-09-06 10:54:31 +1000

    Update footer link

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,14 +6,22 @@ export function Footer() {
       </div>
       <div className="footer">
         <div>
-          <a id="profile-link" href="https://github.com/wwren" target="_blank">
+          <a
+            id="profile-link"
+            href="https://github.com/wwren/Calculator"
+            target="_blank"
+          >
             <i className="fa fa-github">
               <span> GitHub</span>
             </i>
           </a>
         </div>
         <div>
-          <a id="profile-link" href="https://www.linkedin.com/in/ding-ran/" target="_blank>">
+          <a
+            id="profile-link"
+            href="https://www.linkedin.com/in/ding-ran/"
+            target="_blank>"
+          >
             <i className="fa fa-linkedin-square">
               <span> Linkedin</span>
             </i>
